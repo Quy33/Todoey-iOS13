27159922ca94795b25e7c8bb83d2695944970a95
Quy33 <87293639+Quy33@users.noreply.github.com>
2021-08-16 14:47:55 +0700

    Added tableView with data

---
+++ Todoey/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  Todoey
+//
+//  Created by Mcrew Tech on 16/08/2021.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+struct K {
+    static let cellID = "ToDoItemCell"
+}
---
+++ Todoey/TodoLislViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TodoLislViewController: UITableViewController {
+    
+
+    
+    let itemArray = [
+        "Find Milk",
+        "Buy Milk",
+        "Go back Home"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+//MARK: - TableViewDataSourceMethod
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellID, for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+}
+
