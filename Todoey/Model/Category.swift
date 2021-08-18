49435dfe42fe6a1f38dd2854fb0cae64dfe534e9
Quy33 <87293639+Quy33@users.noreply.github.com>
2021-08-18 20:13:58 +0700

    -Readded ChameleonColor for swift 5

--- Todoey/Model/Category.swift
+++ Todoey/Model/Category.swift
@@ -11,5 +11,5 @@ import RealmSwift
 
 class Category: Object {
     @objc dynamic var name : String = ""
-    let items = List<Item>() //Forward Relationship
+    let items = List<Item>() //Forward Relationship  
 }
